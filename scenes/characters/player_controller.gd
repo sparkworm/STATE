9f121f08ee1eca6d379e1fbbef890ea8b9bd8c21
Sparkworm <d.clutter2337@gmail.com>
2025-07-22 18:35:12 -0500

    Implemented CptVelocity to handle velocity of Player as dictated by PlayerController.

--- scenes/characters/player_controller.gd
+++ scenes/characters/player_controller.gd
@@ -1,8 +1,20 @@
 class_name PlayerController
 extends Node
 
+var move_dir: Vector2
+
 @export var player: Character
 @export var body: Body
+@export var cpt_velocity: VelocityComponent
 
 func _physics_process(delta: float) -> void:
-	body.head_look_towards(body.get_global_mouse_position())
+	body.head_and_torso_look_towards(body.get_global_mouse_position())
+	handle_movement(delta)
+
+func handle_movement(delta: float) -> void:
+	cpt_velocity.velocity = player.get_real_velocity()
+	move_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	cpt_velocity.resolve_decel(delta)
+	cpt_velocity.accel(delta, move_dir)
+	player.velocity = cpt_velocity.velocity
+	player.move_and_slide()
