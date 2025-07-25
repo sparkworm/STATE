78452ae10b1bb72bcc9bbdcc236860e4d7570603
Sparkworm <d.clutter2337@gmail.com>
2025-07-25 12:00:40 -0500

    Added gay retard way of detecting collisions, which I'm about to undo

--- scenes/projectiles/bullet_projectile.gd
+++ scenes/projectiles/bullet_projectile.gd
@@ -21,12 +21,25 @@ func _physics_process(delta: float) -> void:
 	position += delta * velocity
 	ray_cast.target_position = old_position - position
 	if ray_cast.is_colliding():
-		handle_collision(ray_cast.get_collider())
+		handle_collision()
 
 ## NOTE: Will need to make sure to get the LAST of all raycast collisions since
 ## the raycast is being sent backwards
-func handle_collision(col: Object) -> void:
-	pass
+func handle_collision() -> void:
+	var last_collider: Object = ray_cast.get_collider()
+	# find the first thing the raycast would have hit (last thing it collides with would be the
+	# first thing it would have hit in this case)
+	while true:
+		ray_cast.add_exception_rid(ray_cast.get_collider_rid())
+		ray_cast.force_raycast_update()
+		var new_collider: Object = ray_cast.get_collider()
+		if new_collider == null:
+			break
+		last_collider = new_collider
+	#last_collider.hit()
+	queue_free()
+
+
 
 func _on_timer_termination_timeout() -> void:
 	queue_free()
