f18d63d29b6f2a32d8f73fc70d04be44d5b8446c
sparkworm <imnota.squirp@gmail.com>
2025-09-05 16:24:28 -0400

    Added reload() function to character, replacing the handle_reload() function that was previously in player_controller.gd

--- scenes/characters/player_controller.gd
+++ scenes/characters/player_controller.gd
@@ -13,6 +13,7 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	body.head_and_torso_look_towards(body.get_global_mouse_position())
+	## TODO: fix rotation of move collision to be more flexible.
 	$"../MoveCollision".rotation = body.torso.rotation
 	handle_movement(delta)
 	
@@ -24,27 +25,7 @@ func _physics_process(delta: float) -> void:
 		elif Input.is_action_pressed("fire"):
 			item._continue_use()
 	if Input.is_action_just_pressed("reload"):
-		handle_reload()
-
-func handle_reload() -> void:
-	var weapon: WieldableGun = player.get_item_held()
-	if weapon == null:
-		print("No weapon to reload")
-	else:
-		if not player.inventory.has_ammo_for_weapon(weapon):
-			print("No ammo for ", weapon.item_name, " reload.")
-		else:
-			# mag reload
-			if weapon.mag_reloadable:
-				var mag: Magazine = player.inventory.get_mag_for_reload(weapon)
-				print(mag)
-				weapon.mag_reload(mag)
-			# round reload
-			else:
-				#player.inventory.individual_ammo[weapon.ID] -= 1 # how did this even work?
-				player.inventory.decrement_ammo(Globals.WeaponAmmo[weapon.ID])
-				weapon.round_reload()
-	MessageBus.update_hud.emit()
+		player.reload()
 
 func handle_movement(delta: float) -> void:
 	cpt_velocity.velocity = player.get_real_velocity()
--- scenes/parent/character.gd
+++ scenes/parent/character.gd
@@ -17,3 +17,29 @@ func _ready() -> void:
 
 func get_item_held() -> Wieldable:
 	return body.get_item_held()
+
+## Handles character's attempt to reload.  Returns whether reload was successful.
+## NOTE: was cut from PlayerController with modifications, so we'll need to make sure nothing
+## horrible happens
+func reload() -> bool:
+	var weapon: WieldableGun = get_item_held()
+	if weapon == null:
+		print("No weapon to reload")
+		return false
+	else:
+		if not inventory.has_ammo_for_weapon(weapon):
+			print("No ammo for ", weapon.item_name, " reload.")
+			return false
+		else:
+			# mag reload
+			if weapon.mag_reloadable:
+				var mag: Magazine = inventory.get_mag_for_reload(weapon)
+				print(mag)
+				weapon.mag_reload(mag)
+			# round reload
+			else:
+				#player.inventory.individual_ammo[weapon.ID] -= 1 # how did this even work?
+				inventory.decrement_ammo(Globals.WeaponAmmo[weapon.ID])
+				weapon.round_reload()
+		MessageBus.update_hud.emit()
+		return true
