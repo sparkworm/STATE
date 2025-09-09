593c601c6476e20e477ec04e0aa3c0fc47d78891
sparkworm <imnota.squirp@gmail.com>
2025-09-09 09:28:34 -0400

    Improved documentation

--- scenes/components/projectile_spawner.gd
+++ scenes/components/projectile_spawner.gd
@@ -1,6 +1,7 @@
+## A node capable of spawning projectiles from its position in a given direction.
+## Placed at tip of gun to spawn bullets.
 class_name ProjectileSpawner
 extends Node2D
-## Spawns a specified projectile on its current position in a specified direction
 
 @export var projectile_scene: PackedScene
 
@@ -10,6 +11,7 @@ extends Node2D
 ## Speed at which the projectile will travel
 @export var speed: float = 500
 
+## Performs setup for the Projectile, then sends it to Level through MessageBus.projectile_spawned
 func spawn_projectile(dir: Vector2) -> void:
 	var new_proj: Projectile = projectile_scene.instantiate()
 	if new_proj == null:
--- scenes/parent/wieldable.gd
+++ scenes/parent/wieldable.gd
@@ -1,7 +1,10 @@
 class_name Wieldable
 extends Node2D
 
+## The identifier of the Wieldable.  Used in various dictionaries in Globals to access certain
+## attributes of the Wieldable.
 @export var ID: Globals.Wieldables
+## The name of the item for UI purposes.
 @export var item_name: String = "unnamed item"
 
 ## Called when the use button is just pressed
