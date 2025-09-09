98ffb5f48646e0b564ff0775f4357319a3f8b88d
Sparkworm <d.clutter2337@gmail.com>
2025-09-09 17:24:46 -0400

    Added spread as an attribute of ProjectileSpawner, allowing for some natural spread whenever a projectile of any kind is spawned.  NOTE: managing spread related to movement/recoil may require calculating spread outside ProjectileSpawner.

--- scenes/components/projectile_spawner.gd
+++ scenes/components/projectile_spawner.gd
@@ -5,11 +5,18 @@ extends Node2D
 
 @export var projectile_scene: PackedScene
 
+## The number of degrees to either side of the specified angle the projectile may travel.
+@export var spread: float = 0.0
+
 @export_category("Projectile attributes")
 ## The damage that the projectile will deal to what it hits
 @export var damage: float
 ## Speed at which the projectile will travel
 @export var speed: float = 500
+
+func _ready() -> void:
+	# convert spread to radians
+	spread = deg_to_rad(spread)
 
 ## Performs setup for the Projectile, then sends it to Level through MessageBus.projectile_spawned
 func spawn_projectile(dir: Vector2) -> void:
@@ -18,6 +25,7 @@ func spawn_projectile(dir: Vector2) -> void:
 		print("WARNING: Non-Projectile attempted to spawn as projectile")
 		return
 	new_proj.position = global_position
-	new_proj.velocity = dir * speed
+	var rot: float = dir.angle() + randf_range(-spread, spread)
+	new_proj.velocity = Vector2.from_angle(rot) * speed
 	new_proj.damage = damage
 	MessageBus.projectile_spawned.emit(new_proj)
